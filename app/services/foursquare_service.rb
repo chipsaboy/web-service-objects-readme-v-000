282fac7378b7e768fcf75b4ccbec4e6002fde6f9
Nathan Campbell <nathancampbell@outlook.com>
2017-10-17 21:07:12 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -21,15 +21,13 @@ class FoursquareService
   end
 
   def search(client_id, client_secret, zipcode)
-    resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['v'] = '20160201'
       req.params['near'] = zipcode
       req.params['query'] = 'coffee'
     end
-
-    body = JSON.parse(resp.body)
 
     body = JSON.parse(@resp.body)
 
