e2b7bda4780b16b0830366b1b6c1ea732e1352b8
Nathan Campbell <nathancampbell@outlook.com>
2017-10-17 21:03:42 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,16 @@
 class SearchesController < ApplicationController
 
   def search
-    foursquare = FoursquareService.new
-    @resp = foursquare.search(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params["zipcode"])
   end
 
   def friends
     foursquare = FoursquareService.new
     @friends = foursquare.friends(session[:token])
+  end
+
+  def foursquare
+    foursquare = FoursquareService.new
+    @resp = foursquare.search(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET'], params["zipcode"])
   end
 
 end
